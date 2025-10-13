60fef32cd9991318e950ba135066156923c14bf9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-10-13 17:48:00 +0100

    refactor: improve layout and styling in CalendarSummary for enhanced visual appeal

--- components/screens/desktop/components/menu-bar/components/calendar-panel/components/calendar-summary.tsx
+++ components/screens/desktop/components/menu-bar/components/calendar-panel/components/calendar-summary.tsx
@@ -21,13 +21,13 @@ export function CalendarSummary({
   onAdjustMonth,
 }: CalendarSummaryProps) {
   return (
-    <div className="flex items-start justify-between gap-6">
-      <div className="bg-[#F4F4F4] rounded-2xl px-4 py-3 flex flex-col items-center">
-        <p className="capitalize gap-1 flex font-bold text-xl">
+    <div className="flex items-start justify-between">
+      <div className="bg-[#F4F4F4] rounded-4xl px-8 py-4 flex flex-col items-center shadow-xl">
+        <p className="capitalize gap-1 flex font-bold items-center text-2xl">
           <span className="text-rose-500">{dayLabel.slice(0, 3)}</span>
           <span className="text-gray-500">{monthLabel.slice(0, 3)}</span>
         </p>
-        <h1 className="flex items-baseline gap-3 text-7xl font-semibold leading-none">
+        <h1 className="text-8xl leading-none font-semibold -mt-1">
           {dayNumber}
         </h1>
       </div>
