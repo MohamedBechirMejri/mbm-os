a59dc7b393cdc148413a1a13839b66c62246a582
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-10-14 18:33:29 +0100

    refactor: move script tag into head for improved HTML structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      </head>
       <body
         className={`${figtree.variable} antialiased font-[Figtree] overflow-hidden overscroll-none`}
       >
