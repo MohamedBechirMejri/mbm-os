495033ac4184d57701c783145da40845862b52a0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-10-15 18:30:09 +0100

    refactor: adjust font size of display label for improved readability

--- components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
+++ components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
@@ -23,7 +23,7 @@ export default function Display() {
         onFocus={() => setIsHovered(true)}
         onBlur={() => setIsHovered(false)}
       >
-        <div className="flex items-center gap-2 text-sm font-semibold text-white/80">
+        <div className="flex items-center gap-2 text-xs font-semibold text-white/80">
           Display
         </div>
 
