2d0d238c558bf61f0f36c5a0e3f68a867def4706
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-10-15 17:07:50 +0100

    refactor: update Display component layout and improve visual elements for better user experience

--- components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
+++ components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
@@ -1,7 +1,6 @@
-import { Sun } from "lucide-react";
+import { Sun, SunDim } from "lucide-react";
 import ElasticSlider from "@/components/ui/elastic-slider";
 import GlassSurface from "@/components/ui/glass-surface";
-import { Slider } from "@/components/ui/slider";
 import { useBrightnessStore } from "@/lib/brightness-store";
 
 export default function Display() {
@@ -11,30 +10,27 @@ export default function Display() {
 
   return (
     <GlassSurface
-      blur={2}
+      blur={1}
       borderRadius={24}
-      className="!w-full !h-max !bg-black/40 col-span-2"
+      className="!w-full !h-max !bjg-black/30 col-span-2 !backdrop-blur-[8px]"
     >
-      <div className="w-full p-2">
+      <div className="w-full p-2 flex flex-col gap-2">
         <div className="flex items-center gap-2 text-sm font-semibold text-white/80">
           Display
         </div>
-        <div className="mt-4 grid gap-3">
-          <div className="flex items-center gap-3">
-            <ElasticSlider
-              leftIcon={<Sun className="w-4 h-4 text-white/60" />}
-              rightIcon={<Sun className="w-4 h-4 text-white/60" />}
-              startingValue={MIN_BRIGHTNESS}
-              defaultValue={brightness}
-              maxValue={100}
-              onChange={setBrightness}
-              className="w-full h-max items-center flex"
-              sliderClassName="bg-white h-1"
-              sliderBackgroundClassName="bg-black h-1"
-              sliderKnobClassName=""
-            />
-          </div>
-        </div>
+
+        <ElasticSlider
+          leftIcon={<SunDim className="w-4 h-4" />}
+          rightIcon={<Sun className="w-4 h-4 " />}
+          startingValue={MIN_BRIGHTNESS}
+          defaultValue={brightness}
+          maxValue={100}
+          onChange={setBrightness}
+          className="w-full h-max items-center flex"
+          sliderClassName="bg-white h-1"
+          sliderBackgroundClassName="bg-black h-1"
+          sliderKnobClassName=""
+        />
       </div>
     </GlassSurface>
   );
