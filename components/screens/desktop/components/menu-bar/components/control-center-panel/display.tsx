bcd99ad14ae8abcfb308de2043e8658178dc8290
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-10-15 17:08:05 +0100

    refactor: remove unnecessary background color class from GlassSurface for cleaner layout

--- components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
+++ components/screens/desktop/components/menu-bar/components/control-center-panel/display.tsx
@@ -12,7 +12,7 @@ export default function Display() {
     <GlassSurface
       blur={1}
       borderRadius={24}
-      className="!w-full !h-max !bjg-black/30 col-span-2 !backdrop-blur-[8px]"
+      className="!w-full !h-max col-span-2 !backdrop-blur-[8px]"
     >
       <div className="w-full p-2 flex flex-col gap-2">
         <div className="flex items-center gap-2 text-sm font-semibold text-white/80">
